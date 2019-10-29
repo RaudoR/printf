d886284c2b89644e30f0a4d773f2d48fa123d28f
RaudoR <rivaspaulino@outlook.com>
2019-10-29 01:52:41 +0000

    updated printf

---
+++ printf.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ * _printf - this function will produce an output according to format
+ * @format: input
+ * Return: length of string
+ */
+
+int _printf(const char *format, ...)
+
+{
+
+	va_list lst;
+	int alpha = 0, bravo = 0;
+
+	va_start(lst, format);
+	while (format[alpha] != '\0')
+	{
+		if (format[alpha] == '%')
+		{
+			alpha++;
+			while (format[alpha] == ' ')
+				alpha++;
+			if (format[alpha] == 's')
+				bravo += print_string(lst);
+			else if (format[alpha] == 'c')
+				bravo += print_char(lst);
+			else if (format += '%')
+				write(1, "%", 1);
+		}
+		else
+		{
+			bravo++;
+			write(1, &format[alpha], 1);
+		}
+		alpha++;
+	}
+	va_end(lst);
+	return (bravo);
+}
