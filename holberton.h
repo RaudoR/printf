d132ed2480f5aa103f02a3320d8795b9c7b191c5
RaudoR <rivaspaulino@outlook.com>
2019-10-29 01:34:09 +0000

    Printf prototype and helper functions

---
+++ holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+
+int _printf(const char *format, ...);
+int print_char(va_list lst);
+int print_string(va_list lst);
+
+#endif
