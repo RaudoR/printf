64790da38935b832bd81ebbc8b47060af9fcb98e
RaudoR <rivaspaulino@outlook.com>
2019-10-29 19:27:35 +0000

    added print_int function

--- printf.c
+++ printf.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+
 /**
  * _printf - this function will produce an output according to format
  * @format: input
@@ -10,7 +11,8 @@ int _printf(const char *format, ...)
 {
 
 	va_list lst;
-	int alpha = 0, bravo = 0;
+	int alpha = 0;
+	unsigned int bravo = 0;
 
 	va_start(lst, format);
 	while (format[alpha] != '\0')
@@ -20,10 +22,12 @@ int _printf(const char *format, ...)
 			alpha++;
 			while (format[alpha] == ' ')
 				alpha++;
-			if (format[alpha] == 's')
-				bravo += print_string(lst);
-			else if (format[alpha] == 'c')
+			if (format[alpha] == 'c')
 				bravo += print_char(lst);
+			else if (format[alpha] == 's')
+				bravo += print_string(lst);
+			else if (format[alpha] == 'd' || format[alpha] == 'i')
+				bravo += print_int(lst);
 			else if (format[alpha] == '%')
 				write(1, "%", 1);
 			else
